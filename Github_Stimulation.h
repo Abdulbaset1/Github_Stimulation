0fcfc9f54040679b6515f997d93302399fcabb64
Abdul Basit <abdulbaset1878@gmail.com>
2024-05-05 01:14:55 +0500

    I have updated this file

--- Github_Stimulation.h
+++ Github_Stimulation.h
@@ -2,4 +2,31 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class repositoryTree
+{
+	string RepositoryName;
+	class File
+	{
+	public:
+		string fileName;
+		File* next;
+		File(string n);
+	};
+	class Commit
+	{
+	public:
+		string commitName;
+		Commit(string n);
 
+	};
+	class treeNode
+	{
+	public:
+		treeNode* root;
+		treeNode* left;
+		treeNode* right;
+		File* Filex;
+		Commit* commit;
+
+	};
+};
